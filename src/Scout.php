74f007d0f5b78f589014899094f7ddb4855b771d
Dries Vints <dries@vints.be>
2024-09-03 17:30:25 +0200

    version

--- src/Scout.php
+++ src/Scout.php
@@ -12,7 +12,7 @@ class Scout
      *
      * @var string
      */
-    const VERSION = '10.11.1';
+    const VERSION = '10.11.2';
 
     /**
      * The job class that should make models searchable.
